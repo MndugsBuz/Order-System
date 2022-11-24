3acbee4e0699accab59c165761dd00bae33b0271
MndugsBuz <mindaugas.bauza@codeacademylt.onmicrosoft.com>
2022-11-24 19:25:01 +0200

    OrderItems Repository Methods Add, Delete, Read and Write to Json, Print List

--- Order System/InterfacePrint.cs
+++ Order System/InterfacePrint.cs
@@ -26,11 +26,12 @@ namespace Order_System
             int menuOption = int.Parse(Console.ReadLine());
             bool menuQuit = true;
 
-            // Read list of orders from JSON file
             OrdersRepository ordersRepository = new OrdersRepository();
             ordersRepository.ReadFromJson();
             CustomerRepository customersRepository = new CustomerRepository();
-            //customersRepository.ReadFromJson();
+            customersRepository.ReadFromJson();
+            OrderItemRepository orderItemsRepository = new OrderItemRepository();
+            //orderItemsRepository.ReadFromJson();
 
             while (menuQuit)
             {
@@ -84,24 +85,24 @@ namespace Order_System
                                     }
                             }
                             break;
-                            
-
+                        
                         }
                     case 3:
                         {
                             Console.WriteLine("Menu:[3] Order Item");
-                            Console.WriteLine("Menu:[3] [1] New Order Item; [2] Delete Order Item");
+                            Console.WriteLine("Menu:[3] [1] New Order Item; [2] Delete Order Item [3] List current order Items");
                             int orderItemAction = int.Parse(Console.ReadLine());
-                            OrderItemRepository orderItemRepository = new OrderItemRepository();
+                            
                             switch (orderItemAction)
                             {
                                 case 1:
-
-                                    orderItemRepository.AddOrderItem();
+                                    orderItemsRepository.AddOrderItem();
                                     break;
                                 case 2:
-                                    Console.WriteLine("[2] Enter Order number to Delete");
-                                    string ordernumberDelete = (Console.ReadLine());
+                                    orderItemsRepository.DeleteOrderItem();
+                                    break;
+                                case 3:
+                                    orderItemsRepository.PrintOrderItems();
                                     break;
                                 default:
                                     {
@@ -155,8 +156,9 @@ namespace Order_System
                         {
                             Console.WriteLine("Menu:[6] Quit the program");
                             menuQuit = false;
-                            ordersRepository.WriteToJson();
                             customersRepository.WriteToJson();
+                            ordersRepository.WriteToJson();
+                            orderItemsRepository.WriteToJson();
                             break;
                         }
 
--- Order System/Repositories/OrderItemRepository.cs
+++ Order System/Repositories/OrderItemRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Order_System.Entities;
 
 namespace Order_System.Repositories
@@ -21,6 +22,23 @@ namespace Order_System.Repositories
           
         }
 
+        public List<OrderItem> WriteToJson()
+        {
+            File.WriteAllText("C:/desktop ml/Renkuosi programuoti/Back-End C Sharp code/221017 code mb paskaita Basic_C#_exam/Order System/Order System/orderitems.json",
+            JsonConvert.SerializeObject(orderItems));
+            Console.WriteLine("OrderItems writed to JSON file");
+
+            return orderItems;
+        }
+
+        public List<OrderItem> ReadFromJson()
+        {
+            string readjson = File.ReadAllText("C:\\desktop ml\\Renkuosi programuoti\\Back-End C Sharp code\\221017 code mb paskaita Basic_C#_exam\\Order System\\Order System\\orderitems.json");
+            orderItems = JsonConvert.DeserializeObject<List<OrderItem>>(readjson);
+
+            return orderItems;
+        }
+
         public List<OrderItem> AddOrderItem()
         {
             Console.WriteLine("[1] Order Number");
@@ -30,18 +48,43 @@ namespace Order_System.Repositories
             Console.WriteLine("[3] Quantity");
             decimal quantity = decimal.Parse(Console.ReadLine());
             orderItems.Add(new OrderItem(orderNumber, productId, quantity));
-                       
+
             return orderItems;
         }
 
-        public List<OrderItem> DeteleCustomer()
+        public List<OrderItem> DeleteOrderItem()
         {
-            Console.WriteLine("[2] Enter Customer number to Delete");
-           // int customernumberDelete = int.Parse(Console.ReadLine());
-           // orderItems.RemoveAt(customernumberDelete - 1);
+            Console.WriteLine("[2] Enter Order number to Delete");
+            string ordernumbertoDelete = Console.ReadLine();
+            foreach (var item in orderItems)
+            {
+
+                if (item.OrderNumber == ordernumbertoDelete)
+                {
+                    Console.WriteLine("Removing order with number: " + item.OrderNumber);
+                    orderItems.Remove(item);
+                    break;
+                }
+            }
 
             return orderItems;
         }
-
+        public void PrintOrderItems()
+        {
+            Console.WriteLine("OrderItems:");
+            int index = 1;
+            foreach (var items in orderItems)
+            {
+                Console.Write(index + ". ");
+                Console.Write(items.OrderNumber + "|");
+                Console.Write(items.ProductId + "|");
+                Console.Write(items.Quantity + "|");
+                Console.WriteLine();
+                index++;
+            }
+            Console.WriteLine();
+        }
     }
 }
+
+   
